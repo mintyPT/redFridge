b15027343b2026c77a3ac0d3133f2c3ec2b40a49
Mauro G. Santos <mintypt@gmail.com>
2011-11-02 09:35:46 +0000

    Version improved a bit

--- fridge.py
+++ fridge.py
@@ -1,31 +1,74 @@
-fridge ={}
+import json
 
-def open_fridge(product, action, amount):
-    if (not(fridge.has_key(product)) and action != sub):
+
+def load():
+    try:
+        f = open("fridge.json","r")
+        fridge = json.load(f)
+        f.close()
+    except :
+        fridge = {}
+    return fridge
+
+
+
+def open_fridge(product, action, amout):
+    if(not(fridge.has_key(product)) and action != "sub"):
         fridge.update({product : 0})
         add_product(product)
     else:
         if action == "add":
             for i in range(amount):
                 add_product(product)
+            save(fridge)
         elif action == "sub":
             for i in range(amount):
                 sub_product(product)
+            save(fridge)
         else:
-            return "Bitte sub oder add ein Produkt!"
-
+            return "Command not recognised!"
 
 def add_product(product):
-    fridge[product] += 1
-    message = "Produkt hinzugefuegt"
-    return message
+    if fridge.has_key(product):
+        fridge[product] += 1
+    else:
+        fridge.update({product : 0})
+        add_product(product)
+    message = product + " added!"
+    return  message
 
 def sub_product(product):
-    if fridge[product] >= 1:
+    if(fridge[product]>=1):
         fridge[product] -= 1
-        message = "Produkt rausgenommen"
+        message = product + " subtracted!"
     else:
-        message = "Fridge leer!"
+        message = "No more product to subtract!"
     return message
+
+def save(fridge):
+    from StringIO import StringIO
+    jfridge = StringIO()
+
+    json.dump(fridge, jfridge)
+    f = open("fridge.json", "w")
+
+    #f.write(jfridge)
+    f.write(jfridge.getvalue())
+    f.close()
+    return "saved!"
+
+def clear(fridge):
+    fridge.clear()
+    save(fridge)
+
+
+def get_amount(product):
+    message = product, "-->", fridge[product]
+    return message
+
+
+fridge = load()
+print "Fridge opened!"
+
 
 
